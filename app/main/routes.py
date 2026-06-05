63b0a8690c94f3ba103e92cf016b25581f05720c
Indrajit Ghosh <indrajitghosh912@gmail.com>
2026-06-05 12:33:48 +0530

    academic cv and professional cv separated and added on the homepage

--- app/main/routes.py
+++ app/main/routes.py
@@ -27,8 +27,14 @@ INSTITUTE_EMAIL = "indrajitg@iitk.ac.in"
 @main_bp.route('/')
 def index():
     institute_email = INSTITUTE_EMAIL
-    indra_cv_link = "https://drive.google.com/file/d/17YPOjnIjTDw-lTRB5h47SqDj_vbOapHz/view?usp=sharing"
-    return render_template('index.html', indra_cv_link=indra_cv_link, institute_email=institute_email)
+    indra_academic_cv_link = "https://drive.google.com/file/d/17YPOjnIjTDw-lTRB5h47SqDj_vbOapHz/view?usp=drive_link"
+    indra_professional_cv_link = "https://drive.google.com/file/d/15SM1JLgfUYDlIaBEoUq_Bv3VFr34TYIG/view?usp=drive_link"
+    return render_template(
+        'index.html', 
+        indra_academic_cv_link=indra_academic_cv_link, 
+        indra_professional_cv_link=indra_professional_cv_link,
+        institute_email=institute_email
+        )
 
 
 #######################################################
